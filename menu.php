a22af02efacc3e51418f3dad2d4210fbe1bd986e
Ruby-J <53419320+Ruby-J@users.noreply.github.com>
2019-07-29 15:58:18 -0400

    Create menu.php

---
+++ menu.php
@@ -0,0 +1,13 @@
+    <div id="uselesssidebar">
+        <a href="/"><div class="dropdown">
+            Home
+        </div></a>
+        <div class="dropdown">
+          Stuff <span id="uparrow" style="float: right;">▲</span> <span id="downarrow" style="float: right;">▼</span>
+          <div class="dropdown-content">
+            <a href="/test">Test</a>
+            <a href="/adal">Game (WIP)</a>
+            <a href="https://rubyjcat.tumblr.com">Tumblr</a>
+          </div>
+        </div>
+    </div> /* useless sidebar */
