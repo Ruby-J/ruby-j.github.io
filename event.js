f5a345a4c0cc77d99cf93cd806e78b8e10df61aa
Ruby-J <53419320+Ruby-J@users.noreply.github.com>
2019-07-29 14:12:53 -0400

    Create event.js

---
+++ event.js
@@ -0,0 +1,14 @@
+      var divHate;
+        
+        function cacheDOMElements() {
+            divHate = document.getElementById("divHate");
+        }
+        
+        cacheDOMElements();
+        
+	    if (flag_0001 === 0){
+            divHate.style.display = "none";
+            }
+        else {
+            divHate.style.display = "";      
+        }
